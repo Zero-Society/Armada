df8b28732fb16938272970206d72955d293c7c47
Selena Chen <selenachendesign@gmail.com>
2018-06-09 02:14:52 -0700

    Orders

---
+++ frontend/js/Orders.js
@@ -0,0 +1,15 @@
+
+
+var vueProducts = new Vue({
+  el: '#orders-parent',
+  data: {
+    orders: []
+  }
+})
+
+axios.get("http://localhost:3000/orders").then(function (result) {
+  console.log(JSON.stringify(result.data));
+
+  vueProducts.orders = result.data;
+  //vueProducts.$set('products', products.data.products);
+});
