9c1dc305c42f108358312cd508e2b3b79176ae08
Alex Yao <alex.yao@berkeley.edu>
2018-06-10 05:26:41 +0800

    iot-code push

--- Hardware/particle-iot/armada-iot/armada-iot.ino
+++ Hardware/particle-iot/armada-iot/armada-iot.ino
@@ -1,37 +1,49 @@
-int led1 = D0;
-int led2 = D7;
+// We will be using D1 to control our LED
+int ledPin = D1;
 
-void setup() {
-  // put your setup code here, to run once:
-   pinMode(led1, OUTPUT);
-   pinMode(led2, OUTPUT);
+// Our button wired to D0
+int buttonPin = D0;
 
-   Particle.function("led",ledToggle);
+void setup()
+{
 
-   digitalWrite(led1, LOW);
-   digitalWrite(led2, LOW);
-   
+  // For input, we define the
+  // pushbutton as an input-pullup
+  // this uses an internal pullup resistor
+  // to manage consistent reads from the device
+
+  pinMode( buttonPin , INPUT_PULLUP); // sets pin as input
+
+  // We also want to use the LED
+
+  pinMode( ledPin , OUTPUT ); // sets pin as output
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+   // find out if the button is pushed
+   // or not by reading from it.
+   int buttonState = digitalRead( buttonPin );
+
+  // remember that we have wired the pushbutton to
+  // ground and are using a pulldown resistor
+  // that means, when the button is pushed,
+  // we will get a LOW signal
+  // when the button is not pushed we'll get a HIGH
+
+  // let's use that to set our LED on or off
+
+  if( buttonState == LOW )
+  {
+    // turn the LED On
+    digitalWrite( ledPin, HIGH);
+  }else{
+    // otherwise
+    // turn the LED Off
+    digitalWrite( ledPin, LOW);
+
+  }
+
 
 }
-
-int ledToggle(String command) {
-    if (command=="on") {
-        digitalWrite(led1,HIGH);
-        digitalWrite(led2,HIGH);
-        return 1;
-    }
-    else if (command=="off") {
-        digitalWrite(led1,LOW);
-        digitalWrite(led2,LOW);
-        return 0;
-    }
-    else {
-        return -1;
-    }
-  
-}
-
