6c92ddf09318a39ac7346f9dd1309dd4e7c7c49f
Kent <khmakishima@ucdavis.edu>
2018-06-08 20:27:08 -0700

    push contract file

---
+++ Backend/smart_contracts/armadasc.cpp
@@ -0,0 +1,11 @@
+#include "common.hpp"
+#include <eosiolib/contract.hpp>
+#include <eosiolib/dispatcher.hpp>
+#include <eosiolib/vector.hpp>
+
+namespace armada {
+   using eosio::action_meta;
+   using eosio::singleton;
+   using eosio::key256;
+   using std::string;
+   using std::vector;
